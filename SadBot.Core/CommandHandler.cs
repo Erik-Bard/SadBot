2a6cac19fed6e3ee72d885de01bd7c709a78ac18
Erik Bard <bard.erik861@gmail.com>
2022-05-21 12:51:59 +0200

    Add project files.

---
+++ SadBot.Core/CommandHandler.cs
@@ -0,0 +1,75 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using SadBot.Core.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadBot.Core
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private IServiceProvider _services;
+
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandsAsync(IServiceProvider services)
+        {
+            // Hook the MessageReceived event into our command handler
+            _client.MessageReceived += HandleCommandAsync;
+            // Either search the program and add all Module classes that can be found.
+            // Module classes MUST be marked 'public' or they will be ignored.
+            // You also need to pass your 'IServiceProvider' instance now,
+            // so make sure that's done before you get here.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+            // Or add Modules manually if you prefer to be a little more explicit:
+            await _commands.AddModuleAsync<InfoModule>(services);
+            await _commands.AddModuleAsync<UserModule>(services);
+            await _commands.AddModuleAsync<GifModule>(services);
+
+            _services = services;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+}
---
+++ SadMemerBot/Program.cs
@@ -0,0 +1,123 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SadBot.Core;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TenorSharp;
+
+namespace SadMemerBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        // Keep the CommandService and DI container around for use with commands.
+        // These two types require you install the Discord.Net.Commands package.
+        private readonly CommandService _commands;
+        private readonly IServiceProvider _services;
+        private readonly IConfiguration _config;
+        private CommandHandler _handler;
+        private Program()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                // How much logging do you want to see?
+                LogLevel = LogSeverity.Info,
+
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                //MessageCacheSize = 50,
+            });
+
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                // Again, log level:
+                LogLevel = LogSeverity.Info,
+
+                // There's a few more properties you can set,
+                // for example, case-insensitive commands.
+                CaseSensitiveCommands = false,
+            });
+
+            // Subscribe the logging handler to both the client and the CommandService.
+            _client.Log += Log;
+            _commands.Log += Log;
+
+            // Setup Environment
+            var builder = new ConfigurationBuilder()
+              .AddJsonFile($"appsettings.json", true, true)
+              .AddUserSecrets<Program>()
+              .AddEnvironmentVariables();
+            _config = builder.Build();
+
+            var tenorKey = _config.GetRequiredSection("TenorKey").Value;
+            // Setup your DI container.
+            _services = ConfigureServices(_client, _commands, tenorKey);
+            _handler = _services.GetRequiredService<CommandHandler>();
+        }
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        // If any services require the client, or the CommandService, or something else you keep on hand,
+        // pass them as parameters into this method as needed.
+        // If this method is getting pretty long, you can seperate it out into another file using partials.
+        private static IServiceProvider ConfigureServices(DiscordSocketClient client, CommandService commandService, string tenorKey)
+        {
+            var map = new ServiceCollection()
+                // Repeat this for all the service classes
+                // and other dependencies that your commands might need.
+                .AddSingleton(new CommandHandler(client, commandService))
+                .AddSingleton(new TenorClient(tenorKey));
+            return map.BuildServiceProvider();
+        }
+
+        public async Task MainAsync()
+        {
+            var myToken = _config.GetRequiredSection("DiscordToken").Value;
+            // Centralize the logic for commands into a separate method.
+            await _handler.InstallCommandsAsync(_services);
+            // Login and connect.
+            await _client.LoginAsync(TokenType.Bot, myToken);
+            await _client.StartAsync();
+
+            // Wait infinitely so your bot actually stays connected.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static Task Log(LogMessage message)
+        {
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
+
+            // If you get an error saying 'CompletedTask' doesn't exist,
+            // your project is targeting .NET 4.5.2 or lower. You'll need
+            // to adjust your project's target framework to 4.6 or higher
+            // (instructions for this are easily Googled).
+            // If you *need* to run on .NET 4.5 for compat/other reasons,
+            // the alternative is to 'return Task.Delay(0);' instead.
+            return Task.CompletedTask;
+        }
+    }
+}
